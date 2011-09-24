6740f2a9760eb022e41ba6367e5a1f9f52062c19
gino <gino.heyman@gmail.com>
2011-09-24 20:11:02 +0200

    Added Test Client test usage test

---
+++ src/Typesafe.Web.Mvc.IntegrationTests/IntegrationTesting/TestClientSpecs/When_sending_an_iis_express_hosted_request.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using NUnit.Framework;
+using Typesafe.Web.Mvc.IntegrationTesting;
+
+namespace Typesafe.Web.Mvc.IntegrationTests.IntegrationTesting.TestClientSpecs
+{
+	[TestFixture]
+	public class When_getting_a_resource_from_an_iis_express_hosted_app
+	{
+		private readonly HttpWebResponse response;
+
+		public When_getting_a_resource_from_an_iis_express_hosted_app()
+		{
+			TestClient.InitializeServer("C:\\temp", 7823);
+			response = TestClient.Get("foo");
+		}
+
+		[Test]
+		public void a_proper_response_should_be_returned()
+		{
+			Assert.IsNotNull(response);
+		}
+	}
+}
--- src/Typesafe.Web.Mvc/IntegrationTesting/TestClient.cs
+++ src/Typesafe.Web.Mvc/IntegrationTesting/TestClient.cs
@@ -2,75 +2,78 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using Typesafe.Web.Mvc.Rest;
 
 namespace Typesafe.Web.Mvc.IntegrationTesting
 {
 	public static class TestClient
 	{
 		private static IisExpress currentIisInstance;
+		private static int currentPort;
 
 		public static void InitializeServer(string physicalDirectory, int port)
 		{
 			if(currentIisInstance != null)
 				currentIisInstance.Stop();
 
+			currentPort = port;
 			currentIisInstance = new IisExpress(physicalDirectory, port);
 		}
 
-		//public static HttpWebResponse Get(string url, IDictionary<string, string> headers = null )
-		//{
-		//    return ExecuteRequest("GET", url, null, headers);
-		//}
+		public static HttpWebResponse Get(string url, IDictionary<string, string> headers = null)
+		{
+			return ExecuteRequest("GET", url, null, headers);
+		}
 
-		//public static HttpWebResponse Post(string url, string body, IDictionary<string, string> headers = null)
-		//{
-		//    return ExecuteRequest("POST", url, body, headers);
-		//}
+		public static HttpWebResponse Post(string url, string body, IDictionary<string, string> headers = null)
+		{
+			return ExecuteRequest("POST", url, body, headers);
+		}
 
-		//private static HttpWebResponse ExecuteRequest(string method, string url, string body, IDictionary<string, string> headers)
-		//{
-		//    var request = (HttpWebRequest)WebRequest.Create("http://localhost:88/" + url);
-		//    request.Method = method;
+		private static HttpWebResponse ExecuteRequest(string method, string url, string body, IDictionary<string, string> headers)
+		{
+			var request = (HttpWebRequest)WebRequest.Create("http://localhost:" + currentPort + "/" + url);
+			request.Method = method;
 
-		//    request.SetHeaders(headers);
-		//    request.SetBody(body);
+			request.SetHeaders(headers);
+			request.SetBody(body);
 
-		//    LogRequest(request, body);
+			LogRequest(request, body);
 
-		//    HttpWebResponse response = null;
+			HttpWebResponse response = null;
 
-		//    try
-		//    {
-		//        response = request.GetResponse() as HttpWebResponse;
-		//    }
-		//    catch (WebException ex)
-		//    {
-		//        response = ex.Response as HttpWebResponse;
-		//    }
+			try
+			{
+				response = request.GetResponse() as HttpWebResponse;
+			}
+			catch (WebException ex)
+			{
+				response = ex.Response as HttpWebResponse;
+			}
 
-		//    LogResponse(response);
+			LogResponse(response);
 
-		//    return response;
-		//}
+			return response;
+		}
 
-		//private static void LogResponse(HttpWebResponse response)
-		//{
-		//    Console.WriteLine("\r\n# RESPONSE");
-		//    Console.WriteLine("HTTP/1.1 {0} {1}", (int)response.StatusCode, response.StatusDescription);
-		//    foreach (var key in response.Headers.AllKeys)
-		//        Console.WriteLine("{0}: {1}", key, response.Headers[key]);
+		private static void LogResponse(HttpWebResponse response)
+		{
+			Console.WriteLine("\r\n# RESPONSE");
+			Console.WriteLine("HTTP/1.1 {0} {1}", (int)response.StatusCode, response.StatusDescription);
+			foreach (var key in response.Headers.AllKeys)
+				Console.WriteLine("{0}: {1}", key, response.Headers[key]);
 
-		//    Console.WriteLine("\r\n" + new StreamReader(response.GetResponseStream()).ReadToEnd());
-		//}
+			Console.WriteLine("\r\n" + new StreamReader(response.GetResponseStream()).ReadToEnd());
+		}
 
-		//private static void LogRequest(HttpWebRequest request, string body)
-		//{
-		//    Console.WriteLine("# REQUEST");
-		//    Console.WriteLine("{0} {1} HTTP/1.1", request.Method, request.RequestUri);
-		//    foreach (var key in request.Headers.AllKeys)
-		//        Console.WriteLine("{0}: {1}", key, request.Headers[key]);
+		private static void LogRequest(HttpWebRequest request, string body)
+		{
+			Console.WriteLine("# REQUEST");
+			Console.WriteLine("{0} {1} HTTP/1.1", request.Method, request.RequestUri);
+			foreach (var key in request.Headers.AllKeys)
+				Console.WriteLine("{0}: {1}", key, request.Headers[key]);
 
-		//    if (body != null) Console.WriteLine("\r\n" + body);
-		//}
+			if (body != null) Console.WriteLine("\r\n" + body);
+		}
 	}
 }
