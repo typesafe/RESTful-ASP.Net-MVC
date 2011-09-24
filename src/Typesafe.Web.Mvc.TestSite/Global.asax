76289e70ff59bad22281cf9f3bee78decc0828e4
gino <gino.heyman@gmail.com>
2011-09-24 18:05:12 +0200

    Added empty/stripped playground/test MVC3 web site.

---
+++ src/Typesafe.Web.Mvc.TestSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Typesafe.Web.Mvc.TestSite.MvcApplication" Language="C#" %>
---
+++ src/Typesafe.Web.Mvc.TestSite/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Typesafe.Web.Mvc.TestSite
+{
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			RegisterGlobalFilters(GlobalFilters.Filters);
+			RegisterRoutes(RouteTable.Routes);
+		}
+
+		private static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+
+		private static void RegisterRoutes(RouteCollection routes)
+		{
+		}
+	}
+}
