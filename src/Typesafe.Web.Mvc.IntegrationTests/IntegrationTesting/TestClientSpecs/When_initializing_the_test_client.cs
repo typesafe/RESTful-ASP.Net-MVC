14e906b0c2ee64a477b27f3943ef907d3ad1eba8
gino <gino.heyman@gmail.com>
2011-09-24 19:47:06 +0200

    Added TestClient integration test

---
+++ src/Typesafe.Web.Mvc.IntegrationTests/IntegrationTesting/TestClientSpecs/When_initializing_the_test_client.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using Typesafe.Web.Mvc.IntegrationTesting;
+
+namespace Typesafe.Web.Mvc.IntegrationTests.IntegrationTesting.TestClientSpecs
+{
+	[TestFixture]
+	public class When_initializing_the_test_client
+	{
+		[Test]
+		public void an_iis_express_instance_should_run()
+		{
+			TestClient.InitializeServer("c:\\temp", 9176);
+
+			Assert.IsTrue(Process.GetProcessesByName("iisexpress").Length == 1);
+		}
+	}
+}
